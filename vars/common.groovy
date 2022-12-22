ad96fc038c48851f5d27429e15919cf0ad4c03de
Raghu K <raghuk.vit@gmail.com>
2022-12-22 08:24:23 +0530

    Update roboshop-jenkins-shared-library/

--- vars/common.groovy
+++ vars/common.groovy
@@ -23,7 +23,7 @@ def compile(appType) {
 
 def codeQuality() {
   stage('Code Quality') {
-    sh "sonar-scanner -Dsonar.qualitygate.wait=true -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.host.url=http://172.31.8.238:9000 -Dsonar.projectKey=${env.COMPONENT}"
+    sh "sonar-scanner -Dsonar.qualitygate.wait=true -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.host.url=http://172.31.8.238:9000 -Dsonar.projectKey=${env.COMPONENT} ${SONAR_OPTS}"
   }
 }
 
