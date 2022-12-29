0a17954f224f08ff4de024bc1f9bfa09328610ab
Raghu K <raghuk.vit@gmail.com>
2022-12-29 08:38:02 +0530

    Update roboshop-jenkins-shared-library/

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -3,48 +3,50 @@ def call() {
 
     properties([
         parameters([
-            [$class: 'ChoiceParameterDefinition',
-             choices: '\ndev\nprod\n',
-             name: 'ENVIRONMENT',
+            [$class     : 'ChoiceParameterDefinition',
+             choices    : '\ndev\nprod\n',
+             name       : 'ENVIRONMENT',
              description: "Choose Environment"
             ],
-            [$class: 'ChoiceParameterDefinition',
-             choices: '\napply\ndestroy\n',
-             name: 'ACTION',
+            [$class     : 'ChoiceParameterDefinition',
+             choices    : '\napply\ndestroy\n',
+             name       : 'ACTION',
              description: "Choose Action"
             ],
         ]),
     ])
 
-    common.checkout()
+    ansiColor('xterm') {
+      common.checkout()
 
-    if ( ! ENVIRONMENT ) {
-      env.ENVIRONMENT = "${env.ENVIRONMENT_DEFAULT}"
-    }
+      if (!ENVIRONMENT) {
+        env.ENVIRONMENT = "${env.ENVIRONMENT_DEFAULT}"
+      }
 
-    if ( ! ACTION ) {
-      env.ACTION = "${env.ACTION_DEFAULT}"
-    }
+      if (!ACTION) {
+        env.ACTION = "${env.ACTION_DEFAULT}"
+      }
 
-    stage('Terraform init') {
-      addShortText background: '#FFFF00', borderColor: '#FFFF00', color: '', link: '', text: "Env : ${ENVIRONMENT} | Action : ${ACTION}"
-      sh '''
+      stage('Terraform init') {
+        addShortText background: '#FFFF00', borderColor: '#FFFF00', color: '', link: '', text: "Env : ${ENVIRONMENT} | Action : ${ACTION}"
+        sh '''
         terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
       '''
-    }
+      }
 
-    stage('Terraform Plan') {
-      sh '''
+      stage('Terraform Plan') {
+        sh '''
         terraform plan -var-file=env/${ENVIRONMENT}.tfvars
       '''
-    }
+      }
 
-    stage('Terraform Apply') {
-      input message: 'Apply ?', ok: 'Yes'
-      sh '''
+      stage('Terraform Apply') {
+        input message: 'Apply ?', ok: 'Yes'
+        sh '''
         terraform apply -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
       '''
-    }
+      }
 
+    }
   }
 }
