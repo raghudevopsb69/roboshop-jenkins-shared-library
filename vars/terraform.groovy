9c3be58de5a5cec45da543686e01a3c772bae333
Raghu K <raghuk.vit@gmail.com>
2022-12-29 08:09:09 +0530

    Update roboshop-jenkins-shared-library/

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -7,12 +7,25 @@ def call() {
       ansiColor('xterm')
     }
 
+    parameters {
+      choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick Environment')
+      choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick Terraform Action')
+    }
+
     stages {
+
+      stage('Terraform Init') {
+        steps {
+          sh '''
+            terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
+          '''
+        }
+      }
 
       stage('Terraform Plan') {
         steps {
           sh '''
-            terraform plan 
+            terraform plan -var-file=env/${ENVIRONMENT}.tfvars
           '''
         }
       }
