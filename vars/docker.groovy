b864ca52efc79b3084880c7bfe3172f4ae34bf27
Raghu K <raghuk.vit@gmail.com>
2023-01-27 08:01:01 +0530

    Update roboshop-jenkins-shared-library/

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,5 +1,6 @@
 def call() {
   node {
+    common.checkout()
     sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 739561048503.dkr.ecr.us-east-1.amazonaws.com'
     sh 'docker build -t ${COMPONENT} .'
     sh 'docker tag ${COMPONENT}:latest 739561048503.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}'
